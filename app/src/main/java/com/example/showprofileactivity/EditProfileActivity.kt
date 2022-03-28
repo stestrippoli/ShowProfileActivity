561679457cea4472dc3e3565b756f6495238e92b
stestrippoli <80058558+stestrippoli@users.noreply.github.com>
2022-03-29 01:17:03 +0200

    edit activity completed

---
+++ app/src/main/java/com/example/showprofileactivity/EditProfileActivity.kt
@@ -0,0 +1,26 @@
+package com.example.showprofileactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuInflater
+import android.view.View
+import kotlinx.android.synthetic.main.activity_edit_profile.*
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+        registerForContextMenu(imagebtn)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.image_menu, menu)
+    }
+}
--- app/src/main/java/com/example/showprofileactivity/MainActivity.kt
+++ app/src/main/java/com/example/showprofileactivity/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_show_profile)
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
